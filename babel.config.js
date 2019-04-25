9cda2016d9cabb1f4b439413b1e88e9cee210e46
lnked <ed.proff@gmail.com>
2019-04-26 02:03:14 +0300

    update

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,17 @@
 module.exports = function (api) {
-  // const test = api.env('test')
-  // const production = api.env('production')
-  // const development = api.env('development')
-  api.cache(false);
+  const development = api.env('development');
 
   return {
     presets: [
       ['@babel/preset-env', {
         modules: false,
-      }]
+      }],
     ],
     plugins: [
       ['@babel/plugin-proposal-decorators', { legacy: true }],
       ['@babel/plugin-proposal-class-properties', { loose: true }],
       ['@babel/plugin-proposal-object-rest-spread'],
     ],
-    comments: false,
+    comments: development,
   }
 }
