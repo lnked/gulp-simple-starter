daed7e43388f1577305482f9dd6dcf4ef2b4be88
Edik Bulikyan <ed.proff@gmail.com>
2019-07-02 02:53:05 +0300

    update

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,14 @@
 module.exports = {
-  "./src/**/*.css": "stylelint",
-  "./src/**/*.scss": "stylelint --syntax=scss",
+  "./src/**/*.css": [
+    "prettier --write",
+    "stylelint --fix --config ./.stylelint-format",
+    "git add"
+  ],
+  "./src/**/*.scss": [
+    "prettier --write",
+    "stylelint --fix --config ./.stylelint-format --syntax scss",
+    "git add"
+  ],
   "./src/**/*.js": ["eslint src/scripts/* --fix", "git add"],
   "./src/**/*.{png,jpeg,jpg,gif,svg}": ["imagemin-lint-staged", "git add"]
 }
