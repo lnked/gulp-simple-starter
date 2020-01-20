11804869282854c4bdff2a2de230a6cd7b75c8af
lnked <ed.proff@gmail.com>
2020-01-21 00:24:34 +0300

    upgrade dependencies

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,13 +2,11 @@ module.exports = {
   "./src/**/*.css": [
     "prettier --write",
     "stylelint --fix --config ./.stylelint-format",
-    "git add"
   ],
   "./src/**/*.scss": [
     "prettier --write",
     "stylelint --fix --config ./.stylelint-format --syntax scss",
-    "git add"
   ],
-  "./src/**/*.js": ["eslint src/scripts/* --fix", "git add"],
-  "./src/**/*.{png,jpeg,jpg,gif,svg}": ["imagemin-lint-staged", "git add"]
+  "./src/**/*.js": "eslint src/scripts/* --fix",
+  "./src/**/*.{png,jpeg,jpg,gif,svg}": "imagemin-lint-staged",
 }
