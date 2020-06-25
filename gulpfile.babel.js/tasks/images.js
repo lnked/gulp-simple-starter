3108f8388bb66b0bdd392e2f8c13be271266498b
lnked <ed.proff@gmail.com>
2020-06-26 02:26:58 +0300

    upgrade dependencies

--- gulpfile.babel.js/tasks/images.js
+++ gulpfile.babel.js/tasks/images.js
@@ -1,11 +1,13 @@
-import { resolve } from 'path'
-import { src, dest } from 'gulp'
-import newer from 'gulp-newer'
-import imagemin from 'gulp-imagemin'
-import browserSync from 'browser-sync'
+import { resolve } from 'path';
+import { src, dest } from 'gulp';
+import gulpif from 'gulp-if';
+import webp from 'gulp-webp';
+import newer from 'gulp-newer';
+import imagemin from 'gulp-imagemin';
+import browserSync from 'browser-sync';
 
-import { staticPath } from '../env'
-import { imagesPath, imageminConfig } from '../config'
+import { staticPath } from '../env';
+import { imagesPath, imageminConfig } from '../config';
 
 const imagesDist = resolve(staticPath, 'img')
 
@@ -13,6 +15,14 @@ export const imagesWatchPaths = [
   `${imagesPath}/*`,
   `${imagesPath}/**/*`,
 ]
+
+const condition = file => {
+  const { history = [] } = file || {};
+  const [filename] = history;
+  const extension = filename.split('.').pop();
+
+  return ['jpg', 'jpeg'].includes(extension);
+}
 
 export default () =>
   src(imagesWatchPaths)
@@ -22,4 +32,9 @@ export default () =>
       name: 'images',
     }))
     .pipe(dest(imagesDist))
-    .on('end', browserSync.reload)
+    .pipe(gulpif(condition, webp({
+      quality: 50,
+      method: 6,
+    })))
+    .pipe(dest(imagesDist))
+    .on('end', browserSync.reload);
