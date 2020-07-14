7e40621e7dbf3c0287672fe569a25863aaec3972
lnked <ed.proff@gmail.com>
2020-07-15 02:33:28 +0300

    upgrade dependencies

--- gulpfile.babel.js/tasks/templates.js
+++ gulpfile.babel.js/tasks/templates.js
@@ -6,7 +6,6 @@ import beautify from 'gulp-beautify'
 import browserSync from 'browser-sync'
 import frontMatter from 'gulp-front-matter'
 import nunjucksRender from 'gulp-nunjucks-render'
-import pathModifier from 'gulp-path-modifier'
 
 import { outputPath, development } from '../env'
 import { publicPath, templatesPath, htmlFormatConfig } from '../config'
@@ -46,19 +45,6 @@ export default () =>
       envOptions: {
         watch: development,
       },
-    }))
-    .pipe(pathModifier('img', (link) => {
-      if (link && /static\//.test(link)) {
-        link = link.replace('static/', '')
-      }
-
-      if (link && /images\//.test(link)) {
-        link = link.replace('images/', 'img/')
-      }
-
-      return link
-        .replace(/^(\.\/|\.\.\/)|\/$/g, '')
-        .replace(/^\/|\/$/g, '')
     }))
     .pipe(beautify.html(htmlFormatConfig))
     .pipe(dest(resolve(outputPath)))
