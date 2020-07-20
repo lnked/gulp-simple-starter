4d7e48b5c6a2d722da9aed94d58933aa8f8c7ccc
lnked <ed.proff@gmail.com>
2020-07-20 11:30:44 +0300

    upgrade dependencies

--- gulpfile.babel.js/env.js
+++ gulpfile.babel.js/env.js
@@ -1,21 +1,23 @@
 import yargs from 'yargs';
 import { resolve, dirname } from 'path';
 
-export const argv = yargs.argv
+export const argv = yargs.argv;
 
-export const isUncss = !!argv.uncss
-export const optimized = !!argv.optimized
-export const production = !!argv.production
-export const development = !!argv.development
+export const isUncss = !!argv.uncss;
+export const optimized = !!argv.optimized;
+export const production = !!argv.production;
+export const development = !!argv.development;
 
-export const mode = production ? 'production' : 'development'
+export const mode = production ? 'production' : 'development';
 
-export const sourceFolder = 'src'
-export const outputFolder = 'dist'
+export const sourceFolder = 'src';
+export const outputFolder = 'dist';
 
-export const rootPath = resolve(dirname(__dirname))
-export const sourcePath = resolve(rootPath, sourceFolder)
-export const outputPath = resolve(rootPath, outputFolder)
-export const staticPath = resolve(outputPath, 'static')
-export const cacheDirectory = resolve(rootPath, '.cache')
-export const nodeModulesPath = resolve(rootPath, 'node_modules')
+export const rootPath = resolve(dirname(__dirname));
+export const sourcePath = resolve(rootPath, sourceFolder);
+export const outputPath = resolve(rootPath, outputFolder);
+export const staticPath = resolve(outputPath, 'static');
+export const cacheDirectory = resolve(rootPath, '.cache');
+export const nodeModulesPath = resolve(rootPath, 'node_modules');
+export const imagesCache = resolve(cacheDirectory, 'images');
+export const imagesOutput = resolve(staticPath, 'img');
--- gulpfile.babel.js/tasks/images.js
+++ gulpfile.babel.js/tasks/images.js
@@ -3,15 +3,15 @@ import { src, dest } from 'gulp';
 import gulpif from 'gulp-if';
 import webp from 'gulp-webp';
 import newer from 'gulp-newer';
+import plumber from 'gulp-plumber';
 import imagemin from 'gulp-imagemin';
 import browserSync from 'browser-sync';
 import tinypng from 'gulp-tinypng';
 
-import { staticPath, optimized, production } from '../env';
+import { imagesCache, imagesOutput, optimized } from '../env';
 import { imagesPath, imageminConfig, environment } from '../config';
 
 const { TINYPNG_API_KEY = '' } = environment;
-const imagesOutput = resolve(staticPath, 'img')
 
 export const imagesWatchGlob = [
   `${imagesPath}`,
@@ -25,25 +25,25 @@ const condition = formats => file => {
   return formats.includes(filename.split('.').pop() || '');
 }
 
-export default () =>
+const webpConfig = {
+  ...(optimized && {
+    quality: 50,
+    method: 6,
+  } || {}),
+};
+
+export default () => {
   src([
     `${imagesPath}/*.*`,
     `${imagesPath}/**/*.*`,
   ])
-    .pipe(newer(imagesOutput))
-    .pipe(gulpif(production, imagemin(imageminConfig, {
-      name: 'images',
-      verbose: true,
-    })))
-    .pipe(dest(imagesOutput))
-
+    .pipe(plumber())
+    .pipe(newer(imagesCache))
+    .pipe(gulpif(optimized, imagemin(imageminConfig, { name: 'images', verbose: true })))
     .pipe(gulpif((optimized && TINYPNG_API_KEY && condition(['png'])), tinypng(TINYPNG_API_KEY)))
+    .pipe(plumber.stop())
+    .pipe(gulpif(condition(['jpg', 'jpeg']), webp(webpConfig)))
+    .pipe(dest(imagesCache));
 
-    .pipe(gulpif(condition(['jpg', 'jpeg']), webp({
-      ...(production && {
-        quality: 50,
-        method: 6,
-      } || {}),
-    })))
-    .pipe(dest(imagesOutput))
-    .on('end', browserSync.reload);
+  return src(`${imagesCache}/**/*.*`).pipe(dest(imagesOutput)).on('end', browserSync.reload);
+}
