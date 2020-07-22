270dfc51432d8179a93ca819b6645d6d90040510
lnked <ed.proff@gmail.com>
2020-07-23 02:48:28 +0300

    upgrade dependencies

--- src/scripts/utils/scroll.ts
+++ src/scripts/utils/scroll.ts
@@ -23,7 +23,10 @@
 // };
 
 export const scroll = function (handleUpdate) {
-  const requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame || function(callback){window.setTimeout(callback, 1000/60)};
+  const requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame ||
+    function(callback){
+      window.setTimeout(callback, 1000/60);
+    };
 
   function loop () {
     handleUpdate(window.pageYOffset, () => {
