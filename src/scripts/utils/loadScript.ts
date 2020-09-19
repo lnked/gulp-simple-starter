f523303d9f4fde374f4ccb9240b4d9cf9085ca2c
lnked <ed.proff@gmail.com>
2020-09-20 01:39:46 +0300

    upgrade dependencies

--- src/scripts/utils/loadScript.ts
+++ src/scripts/utils/loadScript.ts
@@ -1,15 +1,17 @@
 export function loadScript(src, callback) {
   let r = false;
-  const s = document.createElement('script');
-  const t = document.getElementsByTagName('script')[0];
+  const s: any = document.createElement('script');
+  const t: any = document.getElementsByTagName('script')[0];
 
   s.type = 'text/javascript';
   s.src = src;
+
   s.onload = s.onreadystatechange = function() {
     if (!r && (!this.readyState || this.readyState === 'complete')) {
       r = true;
       callback();
     }
   };
-  t.parentNode.insertBefore(s, t);
+
+  t.parentNode?.insertBefore(s, t);
 }
