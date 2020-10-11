7a5f87f678b380b61dbb291e75ad9bfb761f2c4a
lnked <ed.proff@gmail.com>
2020-10-11 15:08:43 +0300

    update

--- gulpfile.babel.js/webpack.config.js
+++ gulpfile.babel.js/webpack.config.js
@@ -1,101 +1,23 @@
+import ESBuildPlugin from 'esbuild-webpack-plugin';
 import webpack from 'webpack';
 import { resolve } from 'path';
-import TerserPlugin from 'terser-webpack-plugin';
-import CompressionPlugin from 'compression-webpack-plugin';
 
-import { mode, production, development, rootPath, cacheDirectory } from './env';
+import { mode, production, rootPath, cacheDirectory } from './env';
 import { scriptsPath } from './config';
 import { getEnvironments } from './get-data';
 
 const scriptsSourcePath = resolve(rootPath, scriptsPath);
 
-const plugins = [
-  new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify(getEnvironments()),
-  }),
-];
-
 const optimizationConfig = {
+  minimize: true,
   minimizer: [
-    new TerserPlugin({
-      test: /\.js(\?.*)?$/i,
-      cache: cacheDirectory,
-      parallel: true,
-      sourceMap: development,
-      terserOptions: {
-        ecma: 5,
-        warnings: false,
-        mangle: true, // Note `mangle.properties` is `false` by default.
-        module: false,
-        toplevel: false,
-        nameCache: null,
-        ie8: false,
-        keep_classnames: undefined,
-        keep_fnames: false,
-        safari10: true,
-        compressor: {
-          warnings: false,
-        },
-        parse: {
-          html5_comments: false,
-        },
-        compress: {
-          ecma: 5,
-          inline: false,
-          sequences: true,
-          comparisons: true,
-          conditionals: true,
-          evaluate: true,
-          booleans: true,
-          loops: true,
-          unused: true,
-          unsafe: false,
-          warnings: false,
-          hoist_funs: true,
-          if_return: true,
-          join_vars: true,
-          dead_code: true,
-          drop_console: production,
-          drop_debugger: production,
-          global_defs: {
-            DEBUG: false,
-          },
-          passes: 5,
-        },
-        output: {
-          ecma: 5,
-          ascii_only: true,
-          comments: false,
-          beautify: false,
-          indent_level: 0,
-        },
-      },
-    }),
+    new ESBuildPlugin(),
   ],
-}
-
-if (production) {
-  plugins.push(
-    new CompressionPlugin({
-      test: /\.js$/,
-      filename: '[path].gz[query]',
-      minRatio: 0.8,
-      threshold: 8192,
-      compressionOptions: {
-        level: 9,
-        verbose: false,
-        verbose_more: false,
-        numiterations: 15,
-        blocksplitting: true,
-        blocksplittingmax: 15,
-      },
-      deleteOriginalAssets: false,
-    }),
-  )
-}
+};
 
 module.exports = {
   mode,
+  target: 'web',
   devtool: production ? false : 'source-map',
   entry: './app',
   output: {
@@ -118,14 +40,6 @@ module.exports = {
         test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
         use: [
-          ...(development && [
-            {
-              loader: 'cache-loader',
-              options: {
-                cacheDirectory,
-              }
-            }
-          ]),
           {
             loader: 'thread-loader',
             options: {
@@ -157,9 +71,12 @@ module.exports = {
       settings: resolve(scriptsSourcePath, 'settings'),
       components: resolve(scriptsSourcePath, 'components'),
     },
-    mainFiles: ['index'],
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
   },
-  plugins,
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(getEnvironments()),
+    }),
+  ],
   optimization: production ? optimizationConfig : {},
 }
