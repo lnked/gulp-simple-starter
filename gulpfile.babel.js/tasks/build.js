3fe6dcfe5c88ab75db8b519717af9bdd1acaeaa5
lnked <ed.proff@gmail.com>
2021-04-25 01:55:09 +0300

    update configs

--- gulpfile.babel.js/tasks/build.js
+++ gulpfile.babel.js/tasks/build.js
@@ -1,6 +1,4 @@
 import { series, parallel } from 'gulp';
 
 export default mode =>
-  series(['clean'], ['images'], parallel('styles', mode), parallel('templates', 'public', 'transfer', 'fonts'), [
-    'critical',
-  ]);
+  series(['clean'], ['images'], parallel('styles', mode), parallel('templates', 'public', 'transfer', 'fonts'));
