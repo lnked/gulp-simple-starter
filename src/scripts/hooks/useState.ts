bfd678b99e758d1baf1addb765c2fec6865e8f06
lnked <esbulikyan@dev.vtb.ru>
2022-08-23 02:36:53 +0300

    upgrade dependencies

--- src/scripts/hooks/useState.ts
+++ src/scripts/hooks/useState.ts
@@ -1,18 +1,21 @@
-type getStateProps = () => any; // eslint-disable-line
-type setStateProps = (value: unknown) => void;
+type setStateProps = (value: unknown, callback: (nextValue: unknown) => void) => void;
 
-type useStateResponse = [getStateProps, setStateProps];
+type useStateResponse = [unknown, setStateProps];
 
-export const useState = (value: unknown): useStateResponse => {
-  let current = value;
+export const useState = (defaultValue: unknown): useStateResponse => {
+  let current = defaultValue;
 
   function getValue() {
     return current;
   }
 
-  function setValue(callback) {
-    current = typeof callback === 'function' ? callback(getValue()) : callback;
+  function setValue(value, callback) {
+    current = value;
+
+    if (typeof callback === 'function') {
+      callback(getValue());
+    }
   }
 
-  return [getValue, setValue];
+  return [current, setValue];
 };
